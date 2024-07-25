4d266b94f8209a7430804556bae03f713f0019cf
RahulKumhar-03 <rahulkumhar03@gmail.com>
2024-07-25 15:43:11 +0530

    linked client with server

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://swarndhara-real-estate.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
