9f6a9f674ff59de4f8f0a49e3570b040780d9b75
RahulKumhar-03 <rahulkumhar03@gmail.com>
2024-07-25 16:07:31 +0530

    updated auth0 config in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-cen1wxneyookra1n.us.auth0.com"
      clientId="gs4Us64Ilk8Dn6PHYVOV7FUAMrr6RsCQ"
      authorizationParams={{
-      redirect_uri:"http://localhost:5173"
+      redirect_uri:"https://swarndhara-real-estate-two.vercel.app"
      }}
      audience="http://localhost:8080"
      scope="openid profile email"
